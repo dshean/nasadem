432abfadad3c5d3a2e87b116ac4e93f230a7292c
David Shean <dshean@gmail.com>
2018-03-25 10:30:16 -0700

    srtm_proc: major update, cleanup

--- srtm_proc.sh
+++ srtm_proc.sh
@@ -2,62 +2,87 @@
 
 #Wrapper to fetch and process NASADEM SRTM products for regional mosaics
 
-#NASADEM hgt_srtmOnly_R4 (non void-filled) are float relative to ellipsoid
-#SRTM-GL1 tiles are relative to EGM96
-#https://lta.cr.usgs.gov/SRTM1Arc
-#February 11-22, 2000
+#SRTM collection dates: February 11-22, 2000
+
+#hgt_srtmOnly_R4 (non void-filled) tiles are float relative to ellipsoid
+#hgt_merge tiles are Int16 relative to EGM96
+
+topdir=/nobackup/deshean/data/nasadem
+if [ ! -d $topdir ] ; then
+    mkdir $topdir
+fi
+#lfs setstripe -c 64 $topdir
+cd $topdir
 
 #For provisional NASADEM products, need NASA Earthdata account
-uname=dshean
+uname=''
 #Need to hardcode password if using GNU parallel to fetch (recommended)
+#Make sure to use hard quotes if pw contains special characters
+#pw=\''pw'\'
 pw=''
+
+#Scripts in repo must be in PATH, or specify here
+srcdir=~/src/nasadem
 
 gdal_opt="-co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER"
 
 #CONUS
-#site='conus'
-#proj='+proj=aea +lat_1=36 +lat_2=49 +lat_0=43 +lon_0=-115 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
+site='conus'
+proj='+proj=aea +lat_1=36 +lat_2=49 +lat_0=43 +lon_0=-115 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 
 #HMA
-site='hma'
-proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
+#site='hma'
+#proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 
 if [ ! -d $site ] ; then
     mkdir $site
 fi
 cd $site
 
-#productdir_list='hgt_srtmOnly hgt_merge err img'
-productdir_list='hgt_srtmOnly'
+#This defines the products to download and process
+productdir_list='hgt_srtmOnly_R4 err_I2 img_comb hgt_merge'
+
+#Filter, adjust vertical datum and reproject
+$srcdir/srtm_post.sh $site "$proj"
+
+exit
 
 for productdir in $productdir_list
 do
-    if ["$productdir" == 'hgt_merge'] ; then
+    echo
+    if [ "$productdir" == 'hgt_merge' ] ; then
         ext_list='hgt num'
-    else if ["$productdir" == 'hgt_srtmOnly_R4'] ; then
+    elif [ "$productdir" == 'hgt_srtmOnly_R4' ] ; then
         ext_list='srtmOnly.hgt'
-    else if ["$productdir" == 'img_comb'] ; then
+    elif [ "$productdir" == 'img_comb' ] ; then
         #ext_list='img img.num'
         ext_list='img'
-    else if ["$productdir" == 'err_I2'] ; then
+    elif [ "$productdir" == 'err_I2' ] ; then
         ext_list='err'
     else
         echo "Invalid product directory"
         exit
     fi
 
-    if [ ! -d $productdir] ; then
+    if [ ! -d $productdir ] ; then
         mkdir $productdir
     fi
     cd $productdir
 
     for ext in $ext_list
     do
+        #Create a subdir, so we avoid overwriting .hdr files for products with multiple file extensions
+        if [ ! -d $ext ] ; then
+            mkdir $ext
+        fi
+        cd $ext
         #Generate a text file containing url for all tiles
         #Edit srtm_tilelist.py, modify lat/lon bounds 
         urllist=${site}_nasadem_tilelist_${productdir}_${ext}.txt
         echo -n > $urllist
-        srtm_tilelist.py $site $productdir >> $urllist 
+        $srcdir/srtm_tilelist.py $site $productdir $ext >> $urllist 
+        nf=$(wc -l $urllist)
+        echo "Total tile count: $nf"
 
         url_zip_list=$(cat $urllist | awk -F'/' '{print $NF}')
         fn_list=$(ls *${ext}.zip)
@@ -69,10 +94,12 @@ do
                 grep $i $urllist >> ${urllist%.*}_missing.txt
             fi
         done
+        nf=$(wc -l ${urllist%.*}_missing.txt)
+        echo "Missing tile count: $nf"
 
         if [ ! -z "$pw" ] ; then 
             #Parallel wget
-            cat ${urllist%.*}_missing.txt | parallel --delay 1 -j 16 --progress "wget --user $uname --password $pw -nc -q {}"
+            cat ${urllist%.*}_missing.txt | parallel --verbose --delay 1 -j 16 --progress "wget --user $uname --password $pw -nc -q {}"
         else
             #Serial wget 
             wget --user $uname --ask-password -nc -i $urllist
@@ -86,39 +113,29 @@ do
                 fn_list_to_unzip+=" $i"
             fi
         done
-        parallel 'unzip {} -d {.}' ::: $fn_list_to_unzip
+
+        if [ ! -z "$fn_list_to_unzip" ] ; then 
+            nf=$(echo $fn_list_to_unzip | wc -w)
+            echo "Unzipped tile count: $nf"
+            parallel --progress 'unzip {} {.}' ::: $fn_list_to_unzip
+        fi
 
         #Generate hdr and prj sidecar files
+        echo "Creating hdr and prj files"
         fn_list=$(ls *.${ext})
-        parallel 'srtm_hdr.sh {}' ::: $fn_list
+        parallel "$srcdir/srtm_hdr.sh {}" ::: $fn_list
 
         #Build mosaic in original WGS84 coordinates
-        gdalbuildvrt ${site}_nasadem_${ext}.vrt $fn_list
-        #gdaladdo_ro.sh ${site}_nasadem_${ext}.vrt
-        #gdal_translate $gdal_opt ${site}_nasadem_${ext}.vrt ${site}_nasadem_${ext}.tif
-        #gdalwarp -overwrite $gdal_opt -r cubic -t_srs "$proj" -tr 30 30 nasadem_${ext}.vrt nasadem_${ext}_30m.tif
-
+        if [ ! -e ${site}_nasadem_${productdir}_${ext}.vrt ] ; then
+            gdalbuildvrt ${site}_nasadem_${productdir}_${ext}.vrt $fn_list
+            #gdaladdo_ro.sh ${site}_nasadem_${ext}.vrt
+            #gdal_translate $gdal_opt ${site}_nasadem_${ext}.vrt ${site}_nasadem_${ext}.tif
+            #gdalwarp -overwrite $gdal_opt -r cubic -t_srs "$proj" -tr 30 30 nasadem_${ext}.vrt nasadem_${ext}_30m.tif
+        fi
         cd ..
     done
+    cd ..
+done
 
-exit
-
-#Mask elevation values where err >= 32769
-productdir=hgt_srtmOnly_R4
-ext=srtmOnly.hgt
-parallel --delay 0.1 'srtm_errmask.py {} err/{/.}.err' ::: $productdir/*.${ext}
-cd $productdir 
-gdalbuildvrt -o ${site}_nasadem_${productdir}_lt5m_err.vrt *{ext}_lt5m_err.tif
-cd ..
-
-#Remove EGM96 offset from void-filled hgt files
-productdir=hgt_merge
-ext=hgt
-parallel --delay 0.1 'dem_geoid --threads 1 --reverse-adjustment {}' ::: $productdir/*.${ext}
-cd $productdir 
-gdalbuildvrt -o ${site}_nasadem_${productdir}_adj.vrt *{ext}-adj.tif
-cd ..
-
-parallel -j 16 "gdalwarp $gdal_opt -overwrite -r cubic -t_srs \"$proj\" -dstnodata -32768 -tr 30 30 {} {.}_aea.tif" ::: $fn_list
-fn_list=$(echo $fn_list | sed 's/.hgt/_aea.tif/g')
-gdalbuildvrt nasadem_hgt_srtmOnly_R4_aea.vrt $fn_list 
+#Filter, adjust vertical datum and reproject
+$srcdir/srtm_post.sh $site "$proj"
