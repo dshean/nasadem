fff0f6ef80c4896694bab31f39adf0cc757add79
David Shean <dshean@gmail.com>
2018-03-25 10:23:57 -0700

    srtm_errmask: minor tweak, apply mm to m scaling in condition

--- srtm_errmask.py
+++ srtm_errmask.py
@@ -11,8 +11,6 @@ import numpy as np
 
 #Max allowable error values in meters
 max_err = 5
-#Note: Units of err are mm
-max_err *= 1000
 
 hgt_fn = sys.argv[1]
 err_fn = sys.argv[2]
@@ -23,7 +21,8 @@ hgt_ds = iolib.fn_getds(hgt_fn)
 hgt = iolib.ds_getma(hgt_ds)
 err = iolib.fn_getma(err_fn)
 
-err[(err > float(max_err))] = np.ma.masked
+#Note: Units of err are mm, multiply by 1000
+err[(err > float(max_err*1000))] = np.ma.masked
 hgt_masked = np.ma.array(hgt, mask=np.ma.getmaskarray(err))
 
 out_fn = os.path.splitext(hgt_fn)[0]+'_lt%sm_err.tif' % max_err
